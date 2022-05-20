15ea180bdb388e8829a5ab55e34faa6b5b533008
Jwychor <53948645+Jwychor@users.noreply.github.com>
2022-05-19 19:25:11 -0500

    Graphs logarithmic gains and prices of crypto

---
+++ README.md
@@ -0,0 +1,16 @@
+# Investment Analysis
+An R and Rmarkdown package analyzing top stocks and crypto returns and prices.
+
+## Installation
+The following code will install the package on your machine.
+
+##### NOTE: This may take a long time given some of the computation times.
+```
+if(!'remotes' %in% installed.packages()){
+  install.packages('remotes', dependencies = TRUE)
+}
+if(!'rlang' %in% installed.packages()){
+  remotes::install_github('r-lib/rlang', dependencies = TRUE)
+}
+remotes::install_github("Jwychor/Crypto_Analysis")
+```
--- app.R
+++ app.R
@@ -1,6 +1,5 @@
 require(tidyquant)
 require(tidyverse)
-require(ggrepel)
 require(plotly)
 require(shiny)
 require(shinydashboard)
@@ -8,22 +7,24 @@ require(shinythemes)
 
 options(scipen = 999)
 
-df <- tq_get(c('BTC-USD', 'SHIB-USD', 'ETH-USD', 'SOL1-USD', 'DOT1-USD', 'ALGO-USD', 'MATIC-USD', 'ADA-USD', 'AMZN', 'TSLA', 'GOOG', 'MSFT'), get = 'stock.prices') %>%
-    filter(date <= today() - 1) %>%
-    group_by(symbol) %>%
-    mutate(first_price = min(adjusted[date == min(date)]),
-           last_price = max(adjusted[date == max(date)])) %>%
-    ungroup() %>%
-    mutate(log2_returns = round(log2(last_price) - log2(adjusted), 2),
-           log2_price = log2(adjusted))
-
+if(!exists('stocks') || max(stocks$date) < today() - 1)
+{
+    stocks <<- tq_get(c('BTC-USD', 'SHIB-USD', 'ETH-USD', 'SOL-USD', 'DOT1-USD', 'ALGO-USD', 'MATIC-USD', 'ADA-USD', 'AMZN', 'TSLA', 'GOOG', 'MSFT', 'NFLX'), get = 'stock.prices') %>%
+        filter(date <= today() - 1) %>%
+        group_by(symbol) %>%
+        mutate(first_price = min(adjusted[date == min(date)]),
+               last_price = max(adjusted[date == max(date)])) %>%
+        ungroup() %>%
+        mutate(log2_returns = round(log2(last_price) - log2(adjusted), 2),
+               log2_price = log2(adjusted))
+}
 ##### UI #####
 ui <- fluidPage(
-    theme = shinytheme('slate'),
+    theme = shinytheme('superhero'),
     headerPanel(
         wellPanel(
             list(HTML('<a href="https://github.com/jwychor"><img src="https://i.ibb.co/n3r8vLx/logo.png" alt="logo" border="0" style="height: 100px; width: 100px;" /></a>','Investment Comparison',
-                      HTML('<br /><h4><a href="https://github.com/Jwychor/Graphing-Top-Bilboard-Data">Source Code</a></h4>'))
+                      HTML('<br /><h4><a href="https://github.com/Jwychor/Investment-Analysis">Source Code</a></h4>'))
             ),
             tags$hr()
         )
@@ -41,20 +42,14 @@ ui <- fluidPage(
         selectizeInput(
             inputId = 'symbols',
             label = 'Select Tickers',
-            choices = unique(df$symbol),
-            select = c('BTC-USD', 'ETH-USD', 'SOL1-USD', 'TSLA', 'GOOG'),
+            choices = unique(stocks$symbol),
+            select = c('BTC-USD', 'ETH-USD', 'SOL-USD', 'TSLA', 'GOOG'),
             multiple = TRUE
         ),
         
-        
-        fluidRow(
-            tags$style('min-height: 90vh;'),
-            column(12,
-                   titlePanel("Chart"),
-                   plotlyOutput("log2_returns_chart",
-                                height = '70vh')
-            )
-        )
+       titlePanel("Chart"),
+       plotlyOutput("log2_returns_chart",
+                    height = '75vh')
     )
 )
 
@@ -64,17 +59,20 @@ server <- function(input, output, session){
     
     ####Outputs####
     output$log2_returns_chart <- renderPlotly({
-        plot_ly(df, x =~ date, y =~ get(input$metrics), color =~ symbol) %>%
+        plot_ly(stocks, x =~ date, y =~ get(input$metrics), color =~ symbol) %>%
             filter(symbol %in% input$symbols) %>%
             group_by(symbol) %>%
-            add_lines() %>%
             layout(plot_bgcolor = '#404040',
                    paper_bgcolor = '#404040',
                    font = list(color = '#FFFFFF'),
-                   xaxis = list(title = 'Date'),
-                   yaxis = list(title = 'Log2 Returns/Price'))
+                   xaxis = list(title = 'Date',
+                                gridcolor = 'black'), 
+                   yaxis = list(title = input$metrics,
+                                gridcolor = 'black',
+                                zerolinecolor = 'black',
+                                zerolinewidth = 2)) %>%
+            add_lines()
     })
 }
 
 shinyApp(ui, server)
-
