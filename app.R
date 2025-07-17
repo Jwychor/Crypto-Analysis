1c3bacacc0587ac9391c762ca803354d120b2926
Jwychor <53948645+Jwychor@users.noreply.github.com>
2025-07-17 13:32:36 -0500

    Can now add additional tickers in UI

--- app.R
+++ app.R
@@ -8,18 +8,46 @@ require(quantmod)
 
 options(scipen = 999)
 
+base_stock_list <- c('BTC-USD', 'SHIB-USD', 'ETH-USD', 'SOL-USD', 'ALGO-USD', 'MATIC-USD', 'AMZN', 'TSLA', 'GOOG', 'MSFT', 'NFLX',
+                     'RIOT', 'MARA', 'CIFR', 'HUT', 'WULF', 'BITF', 'CLSK', 'GLD', 'NVDA')
+
+
+dataRefresh <- function(newTickers = NULL, getBaseData = FALSE){
+  stock_list <- c(newTickers)
+  
+  if(getBaseData){
+    stock_list <- c(stock_list, base_stock_list)
+  }
+  
+  stock_list <- stock_list[order(stock_list)]
+  
+  tryCatch({
+    newStocks <- tq_get(stock_list, get = 'stock.prices') %>%
+      filter(date <= today() - 1) %>%
+      group_by(symbol) %>%
+      mutate(symbol_max_date = max(date),
+             symbol_min_date = min(date),
+             first_price = min(adjusted[date == symbol_min_date]),
+             last_price = max(adjusted[date == symbol_max_date])) %>%
+      ungroup() %>%
+      mutate(log2_returns = round(log2(last_price) - log2(adjusted), 2),
+             log2_price = log2(adjusted))
+    
+    if(getBaseData){
+      stocks <<- newStocks
+      }
+    else {
+      stocks <<- stocks %>% union_all(newStocks)
+      }
+    },
+    error = function(e){
+      message('Error fetching data: ')
+      print(e)
+  })
+}
 if(!exists('stocks') || max(stocks$date) < today() - 1)
 {
-    stocks <<- tq_get(c('BTC-USD', 'SHIB-USD', 'ETH-USD', 'SOL-USD', 'ALGO-USD', 'MATIC-USD', 'AMZN', 'TSLA', 'GOOG', 'MSFT', 'NFLX'), get = 'stock.prices') %>%
-        filter(date <= today() - 1) %>%
-        group_by(symbol) %>%
-        mutate(symbol_max_date = max(date),
-               symbol_min_date = min(date),
-               first_price = min(adjusted[date == symbol_min_date]),
-               last_price = max(adjusted[date == symbol_max_date])) %>%
-        ungroup() %>%
-        mutate(log2_returns = round(log2(last_price) - log2(adjusted), 2),
-               log2_price = log2(adjusted))
+  dataRefresh(getBaseData = TRUE)
 }
 
 ##### UI #####
@@ -40,10 +68,11 @@ ui <- fluidPage(
                     # Stock Symbols
                     selectizeInput(
                       inputId = 'symbols',
-                      label = 'Select Tickers',
+                      label = 'Select Tickers (Type to Add New Tickers)',
                       choices = unique(stocks$symbol),
-                      select = c('BTC-USD', 'ETH-USD', 'TSLA', 'GOOG', 'MSFT', 'AMZN', 'NFLX'),
-                      multiple = TRUE
+                      select = c('AMZN', 'BTC-USD', 'GOOG', 'MSFT', 'NFLX', 'TSLA', 'GLD', 'NVDA'),
+                      multiple = TRUE,
+                      options = list(create = TRUE)
                     ),
                     # Metrics
                     fixedRow(
@@ -59,65 +88,86 @@ ui <- fluidPage(
                       column(3,
                         selectizeInput(
                           inputId = 'highlightSymbol',
-                          label = 'Highlight a Symbol',
-                          choices = c("None", c('BTC-USD', 'ETH-USD', 'TSLA', 'GOOG', 'MSFT', 'AMZN', 'NFLX')),
-                          select = "None",
-                          multiple = FALSE
+                          label = 'Highlighted Symbols',
+                          choices = c('AMZN', 'BTC-USD', 'GOOG', 'MSFT', 'NFLX', 'TSLA', 'GLD', 'NVDA'),
+                          multiple = TRUE
                         )
                       ),
                     ),
 
-                    titlePanel("Chart"),
+                    titlePanel(""),
                     plotlyOutput("log2_returns_chart",
-                                 height = '75vh')
+                                 height = '70vh')
                   ) 
                 ),
                 tabPanel('Debug',
                   wellPanel(
-                    htmlOutput("sessionInfo"),
-                    htmlOutput("debug")
+                    htmlOutput("sessionInfo")
                   )
                 )
-    )
+    ),
+    uiOutput("highlightSymbol")
 )
 
 ##### Server #####
 server <- function(input, output, session){
     t1 <- list(size = 14, color = 'white', family = 'Ariel')
     
+    # update highlight dropdown
+    observeEvent(input$symbols,{
+      # if any new tickers, get new data
+      if(!all(input$symbols %in% unique(stocks$symbol))){
+        newTickers <- input$symbols[!(input$symbols %in% stocks$symbol)]
+        
+        dataRefresh(newTickers)
+        
+        print(paste0("new ticker(s): ", paste(newTickers), " added"))
+      }
+      
+      symb <- as.character(input$symbols)
+      symb <- symb[order(symb)]
+      
+      updateSelectInput(session,
+                        "highlightSymbol", 
+                        label = 'Highlighted Symbols',
+                        choices = c(symb),
+                        selected = input$highlightSymbol)
+    })
+    
     ####Outputs####
     output$log2_returns_chart <- renderPlotly({
+      highlightSymbol <- ""
+      if(!is.null(input$highlightSymbol)){
+        highlightSymbol <- input$highlightSymbol
+      }
+      
       stocks %>%
-      mutate(thickness = case_when(symbol == input$highlightSymbol ~ 30, T ~ 2)) %>%
-      plot_ly(x =~ date, y =~ get(input$metrics), color =~ symbol, line = list(width =~ thickness)) %>%
-          filter(symbol %in% input$symbols) %>%
-          group_by(symbol) %>%
-          layout(plot_bgcolor = '#404040',
-                 paper_bgcolor = '#404040',
-                 font = list(color = '#FFFFFF'),
-                 xaxis = list(title = 'Date',
-                              gridcolor = 'black'),
-                 yaxis = list(title = input$metrics,
-                              gridcolor = 'black',
-                              zerolinecolor = 'black',
-                              zerolinewidth = 4),
-                 tickvals = seq(1, 10, 1)) %>%
-          add_lines()
+        mutate(thickness = case_when(symbol %in% highlightSymbol ~ 3, T ~ 1)) %>%
+        filter(symbol %in% input$symbols) %>%
+        arrange(symbol) %>%
+        plot_ly(x =~ date, 
+                y =~ get(input$metrics),
+                color =~ symbol,
+                mode = 'lines',
+                split =~ thickness,
+                line = list(width =~ thickness)
+        ) %>%
+        layout(plot_bgcolor = '#404040',
+               paper_bgcolor = '#404040',
+               font = list(color = '#FFFFFF'),
+               xaxis = list(title = 'Date',
+                            gridcolor = 'black'),
+               yaxis = list(title = input$metrics,
+                            gridcolor = 'black',
+                            zerolinecolor = 'black',
+                            zerolinewidth = 4,
+                            tickvals = seq(1, 10, 1))) %>%
+        add_lines(name =~ symbol)
     })
-    # update highlight dropdown
-    output$selectedSymbols <- renderUI({
-      selectInput("highlightSymbol", 
-                  label = 'Highlight a Symbol',
-                  choices = as.character(input$symbols),
-                  selected = input$highlightSymbol)
-    })
+    
     # debug
     output$sessionInfo <- renderPrint({
       capture.output(sessionInfo())
-    })
-    
-    output$debug <- renderprint({
-      c(input$highlightSymbol, input$symbols)
     })
 }
 
